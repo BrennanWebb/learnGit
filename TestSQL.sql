7c4b0f48a22801e4eb2e4043da43597ac49d5c8c
Brennan Webb <31872184+BrennanWebb@users.noreply.github.com>
2021-01-21 12:25:34 -0500

    Create TestSQL.sql

---
+++ TestSQL.sql
@@ -0,0 +1,13 @@
+alter proc dbo.test
+as
+set nocount on;
+declare @sql varchar(max), @i int =1
+
+while @i<=10
+  begin
+    set @sql='Select '+cast(@i as varchar(5));
+    exec (@sql);
+    set @i=@i+1
+  end;
+  
+return;
